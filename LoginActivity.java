6cc7b1b267eadf6b55f437b4d7bc18337e47c7a1
Pranav Rasankar <73011807+PranavRasso@users.noreply.github.com>
2022-09-13 20:40:56 +0530

    Add files via upload

---
+++ LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.erpapplicationtrial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        b1=findViewById(R.id.signin);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent l1=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(l1);
+            }
+        });
+    }
+}
